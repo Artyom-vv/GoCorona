37355603f8f0219e670e5af36bca90f52f99399a
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2021-11-30 13:20:00 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@ window.onload = () => {
 	// burger
 	const burger = document.querySelector('.burger');
 	const menu = document.querySelector('.menu');
+	const body = document.body
 
-	burger.addEventListener('click', ()=> {
+	burger.addEventListener('click', () => {
 		burger.classList.toggle('active');
 		menu.classList.toggle('active');
+
+		if (burger.classList.contains('active')) {
+			body.style.height = "100vh";
+			body.style.overflowY = "hidden";
+		} else {
+			body.style.height = "auto";
+			body.style.overflowY = "scroll";
+		}
 	})
 }
